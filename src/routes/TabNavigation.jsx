ff80e6aaa0452ac025988519d28f0d45c20a3f4d
Guilherme DEV <guilhermervisgueira@gmail.com>
2025-09-25 15:51:10 -0300

    criando rotas e screens

---
+++ src/routes/TabNavigation.jsx
@@ -0,0 +1,9 @@
+import { createBottomTabNavigator } from '@react-native/bottom-tabs'
+
+
+import Login from '../screens/Login';
+import Cadastro from '../screens/Cadastro';
+
+const Tab = createBottomTabNavigator();
+
+
